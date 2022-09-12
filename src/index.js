17fb45a640663e6cbb0c49542832f47d0dc8ac20
Sujong Chae <phyzee@naver.com>
2022-09-12 16:34:16 +0900

    test deploy

--- src/index.js
+++ src/index.js
@@ -7,6 +7,9 @@ import GlobalStyle from "./style/GlobalStyle";
 import { ThemeProvider } from "styled-components";
 import { theme, mixins } from "./style/theme";
 
+// how to deploy
+// https://github.com/gitname/react-gh-pages
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <>
